db115dd89f1edd99da9d19b7da1e4a317ca91f41
Wojciech Prokopek <w.prokopek@gmail.com>
2022-04-22 06:46:31 +0200

    adding functional component NavBarItem with passing  dynamic items from list using map()

---
+++ client/Vite App_files/main.jsx
@@ -0,0 +1,19 @@
+var _jsxFileName = "/Users/wojtek/code/web3/web3-cryptoSend/client/src/main.jsx";
+import __vite__cjsImport0_react from "/node_modules/.vite/deps/react.js?v=a6dccc6b"; const React = __vite__cjsImport0_react.__esModule ? __vite__cjsImport0_react.default : __vite__cjsImport0_react;
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=a6dccc6b"; const ReactDOM = __vite__cjsImport1_reactDom_client.__esModule ? __vite__cjsImport1_reactDom_client.default : __vite__cjsImport1_reactDom_client;
+import App from "/src/App.jsx?t=1650568343061";
+import "/src/index.css?t=1650484217064";
+import __vite__cjsImport4_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=a6dccc6b"; const _jsxDEV = __vite__cjsImport4_react_jsxDevRuntime["jsxDEV"];
+ReactDOM.createRoot(document.getElementById("root")).render(/* @__PURE__ */ _jsxDEV(React.StrictMode, {
+  children: /* @__PURE__ */ _jsxDEV(App, {}, void 0, false, {
+    fileName: _jsxFileName,
+    lineNumber: 8,
+    columnNumber: 5
+  }, this)
+}, void 0, false, {
+  fileName: _jsxFileName,
+  lineNumber: 7,
+  columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQUFBO0FBQ0E7QUFDQTtBQUNBOztBQUVBQSxTQUFTQyxXQUFXQyxTQUFTQyxlQUFlLE1BQXhCLENBQXBCLEVBQXFEQyxPQUNuRCx3QkFBQyxNQUFNLFlBQVA7QUFBQSxZQUNFLHdCQUFDLEtBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQURGO0FBQUE7QUFBQTtBQUFBO0FBQUEsUUFERiIsIm5hbWVzIjpbIlJlYWN0RE9NIiwiY3JlYXRlUm9vdCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwic291cmNlcyI6WyIvVXNlcnMvd29qdGVrL2NvZGUvd2ViMy93ZWIzLWNyeXB0b1NlbmQvY2xpZW50L3NyYy9tYWluLmpzeCJdLCJmaWxlIjoiL1VzZXJzL3dvanRlay9jb2RlL3dlYjMvd2ViMy1jcnlwdG9TZW5kL2NsaWVudC9zcmMvbWFpbi5qc3giLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnXG5pbXBvcnQgUmVhY3RET00gZnJvbSAncmVhY3QtZG9tL2NsaWVudCdcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAnXG5pbXBvcnQgJy4vaW5kZXguY3NzJ1xuXG5SZWFjdERPTS5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykpLnJlbmRlcihcbiAgPFJlYWN0LlN0cmljdE1vZGU+XG4gICAgPEFwcCAvPlxuICA8L1JlYWN0LlN0cmljdE1vZGU+XG4pXG4iXX0=
